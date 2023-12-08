6bd0eecb68de3f36acc3fa251e13240d7bbaf05e
Dharamvir Yadav <dharamvir@Dharamvirs-MBP.lan>
2023-12-08 10:40:00 -0600

    Initialize git

---
+++ js/main.js
@@ -0,0 +1,65 @@
+var projectsdata = null
+
+jQuery(document).ready(function($) {
+    console.log('document loaded')
+    $(".scroll").click(function(event){		
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top - 40},1000);
+    });
+
+    $.getJSON('json/projects.json', function(){}).done(function(data){
+        if (data) {
+            projectsdata = data
+            getProjectsData("mobile_proj", "mobile")
+        }
+        
+    })
+});
+
+function getProjectsData(id, type){
+    switch(type) {
+        case 'mobile':
+            if(projectsdata && projectsdata.mobile_projects){
+                var mobile_data = projectsdata.mobile_projects.sort((a, b) => parseFloat(b.id) - parseFloat(a.id));
+                getProjectsUI(id, mobile_data)
+            }
+            break
+        case 'web':
+            if(projectsdata && projectsdata.web_projects){
+                getProjectsUI(id, projectsdata.web_projects)
+            }
+            break
+        case 'machine':
+                if(projectsdata && projectsdata.machine_projects){
+                    getProjectsUI(id, projectsdata.machine_projects)
+                }
+                break
+        default:
+            alert("Nothing found")
+    }
+}
+
+function getProjectsUI(id, data) {
+    if(data.length > 0){
+        $(".projects_tab").removeClass( "active");
+        $( "#"+id+" span" ).addClass( "active" );
+        $(".portfolio__items-container .card-deck").empty()
+        $.each(data, function(i, item) {
+            if(item){
+                $(".portfolio__items-container .card-deck").append(
+                    '<a target="_blank" href='+item.url+'>'+
+                        '<div class="card">'+
+                            '<img class="card-img-top" src='+item.imgsrc+' alt='+item.title+'>'+
+                        '</div>'+
+                        '<div class="card-footer">'+
+                            '<div class="card-title">'+
+                                '<h3>'+item.title+'</h3>'+
+                            '</div>'+
+                            '<small class="text-muted">'+item.tools+'</small>'+
+                        '</div>'+
+                    '</a>'
+                );          
+            }
+        });
+    }
+}
